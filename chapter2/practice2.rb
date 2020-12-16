c2537229da50cfaf354324df0bf4685e647db50a
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-12-16 22:00:09 +0900

    [Finish]chapter2

--- chapter2/practice2.rb
+++ chapter2/practice2.rb
@@ -9,3 +9,31 @@ end
 
 greeting #=> "こんにちは"
 greeting('us') #=> "hello"
+
+
+# ?で終わるメソッド
+#空配列であればtrue, そうでなければfalse
+''.empty? #=> true
+'abc'.empty? #=> false
+
+#引数の文字列が含まれていればtrue, そうでなければfalse
+'watch'.include?('at')  #=> true
+'watch'.include?('in') #=> false
+
+#オブジェクトがnilであればtrue, そうでなければfalse
+nil.nil? #=> true
+'abc'.nil? #=> false
+1.nil? #=> false
+
+#3の倍数ならtrue, それ以外はfalse
+def multiple_of_three?(n)
+    n % 3 == 0
+end
+multiple_of_three?(4) #=> false
+multiple_of_three?(5) #=> false
+multiple_of_three?(5) #=> true
+
+#!で終わるメソッド
+
+
+
